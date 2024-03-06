40a781a7ef6ec1df85691492d391fbcc8b2d8ae6
tdanner <Tdanner@tza.com>
2024-03-06 15:50:01 -0700

    added code

--- Starter_Code/pychain.py
+++ Starter_Code/pychain.py
@@ -49,6 +49,7 @@ import hashlib
 # @TODO
 # Create a Record Data Class that consists of the `sender`, `receiver`, and
 # `amount` attributes
+#done?
 @dataclass
 class Record:
     sender: str
@@ -72,6 +73,7 @@ class Block:
 
     # @TODO
     # Rename the `data` attribute to `record`, and set the data type to `Record`
+    #done?
     record: Record
 
     creator_id: int
@@ -170,19 +172,23 @@ pychain = setup()
 
 # @TODO:
 # Delete the `input_data` variable from the Streamlit interface.
-input_data = st.text_input("Block Data")
+#done?
+# input_data = st.text_input("Block Data")
 
 # @TODO:
 # Add an input area where you can get a value for `sender` from the user.
-# YOUR CODE HERE
+#done?
+sender = st.text_input("Sender")
 
 # @TODO:
 # Add an input area where you can get a value for `receiver` from the user.
-# YOUR CODE HERE
+#done?
+receiver = st.text_input("Receiver")
 
 # @TODO:
 # Add an input area where you can get a value for `amount` from the user.
-# YOUR CODE HERE
+#done?
+amount = st.text_input("Amount")
 
 if st.button("Add Block"):
     prev_block = pychain.chain[-1]
@@ -192,10 +198,12 @@ if st.button("Add Block"):
     # Update `new_block` so that `Block` consists of an attribute named `record`
     # which is set equal to a `Record` that contains the `sender`, `receiver`,
     # and `amount` values
+    #done?
     new_block = Block(
-        data=input_data,
+        # data=input_data,
         creator_id=42,
-        prev_hash=prev_block_hash
+        prev_hash=prev_block_hash,
+        record = Record
     )
 
     pychain.add_block(new_block)
